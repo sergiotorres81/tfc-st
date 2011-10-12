d1b8fef9f62a8fa4c04900add45edd8d01b17b6f
sergiotorres81@gmail.com <sergiotorres81@gmail.com@e5a0838a-8a8e-4bdd-b3e2-6b6e8bd2478c>
2011-10-12 19:19:09 +0000

    Format code for second practice.

--- Practica2/src/client/StubB.java
+++ Practica2/src/client/StubB.java
@@ -1,39 +1,44 @@
 package client;
 
-import commons.*;
+import utils.IOperations;
 
 /**
- * Stub que implementa en el lado del cliente las operaciones de objetos de claseB
+ * Stub que implementa en el lado del cliente las operaciones de objetos de
+ * claseB
+ * 
  * @author Sergio Torres
- *
+ * 
  */
-public class StubB extends StubA implements IOperations{
-	public StubB(){
-		super();
-	}
-	public StubB(String host,int port, int idObjeto, int idSkeleton){
-		super(host,port,idObjeto,idSkeleton);
-	}
-	public int resta(String op1, String op2) {
-		connect();
-		mar.putInt(idObjeto);
-		mar.putInt(idSkeleton);
-		mar.putInt(4); 			// Resta tiene asignado el id de operación 4
-		mar.putString(op1);
-		mar.putString(op2);
-		int res = unmar.getInt();
-		disconnect();		
-		return res;		
-	}
-	public int suma(String op1, String op2) {
-		connect();
-		mar.putInt(idObjeto);
-		mar.putInt(idSkeleton);
-		mar.putInt(3);		 // Suma tiene asignado el id de operación 3
-		mar.putString(op1);
-		mar.putString(op2);		
-		int res = unmar.getInt();		
-		disconnect();		
-		return res;
-	}
+public class StubB extends StubA implements IOperations {
+    public StubB() {
+	super();
+    }
+
+    public StubB(String host, int port, int idObjeto, int idSkeleton) {
+	super(host, port, idObjeto, idSkeleton);
+    }
+
+    public int resta(String op1, String op2) {
+	connect();
+	mar.putInt(idObjeto);
+	mar.putInt(idSkeleton);
+	mar.putInt(4); // Resta tiene asignado el id de operación 4
+	mar.putString(op1);
+	mar.putString(op2);
+	int res = unmar.getInt();
+	disconnect();
+	return res;
+    }
+
+    public int suma(String op1, String op2) {
+	connect();
+	mar.putInt(idObjeto);
+	mar.putInt(idSkeleton);
+	mar.putInt(3); // Suma tiene asignado el id de operación 3
+	mar.putString(op1);
+	mar.putString(op2);
+	int res = unmar.getInt();
+	disconnect();
+	return res;
+    }
 }
