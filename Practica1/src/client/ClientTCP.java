609b46f68b1b3de28e2d6863cfa9fc77da3eefb6
sergiotorres81@gmail.com <sergiotorres81@gmail.com@e5a0838a-8a8e-4bdd-b3e2-6b6e8bd2478c>
2011-10-12 05:39:37 +0000

    ResourceManager en programa principal. Formato del código.

--- Practica1/src/client/ClientTCP.java
+++ Practica1/src/client/ClientTCP.java
@@ -4,68 +4,70 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
-import utils.ResourceManager;
 /**
  * Implementación un cliente TCP
+ * 
  * @author Sergio Torres
  * 
  */
-public class ClientTCP implements IClient{
-	private String host= "localhost";
-	private int port = 8080;
-	private Socket socketCliente;
-	private InputStream inputChannel;
-	private OutputStream outputChannel;
-	private ResourceManager resourceManager;
-	
-	
-	public ClientTCP(String path) {
-		resourceManager = new ResourceManager(path);
-		port = new Integer(resourceManager.getResource("port"));
-		host =resourceManager.getResource("port");
+public class ClientTCP implements IClient {
+    private String host = "localhost";
+    private int port = 8080;
+    private Socket socketCliente;
+    private InputStream inputChannel;
+    private OutputStream outputChannel;
+
+    /**
+     * Constructor de la clase. Instancia el socket de comunicaciones con el
+     * servidor.
+     * 
+     * @param host
+     *            Dirección donde está el servidor
+     * @param port
+     *            Puerto en el que escucha el servidor
+     */
+    public ClientTCP(String host, int port) {
+
+	this.host = host;
+	this.port = port;
+    }
+
+    public void connect() {
+	try {
+	    socketCliente = new Socket(host, port);
+	} catch (Exception e) {
+	    System.out.println("Error de conexión");
 	}
-	/**
-	 * Constructor de la clase. Instancia el socket de comunicaciones con el servidor.
-	 * @param host Dirección donde está el servidor
-	 * @param port Puerto en el que escucha el servidor
-	 */
-	public ClientTCP(String host, int port) {
-		
-		this.host = host;
-		this.port = port;
+    }
+
+    public void sendByte(int b) {
+	try {
+	    outputChannel = socketCliente.getOutputStream();
+	    outputChannel.write(b);
+	} catch (Exception e) {
+	    System.out.println("Error en envío / recepción de datos");
 	}
-	public void connect(){
-		try {
-			socketCliente = new Socket(host, port);
-		} catch (Exception e) {
-			System.out.println("Error de conexión");
-		}
+	System.out.println("Envío: " + b); // La información hay que obtenerla
+					   // fuera del try / catch
+    }
+
+    public int receiveByte() {
+	int b = 0;
+	try {
+	    inputChannel = socketCliente.getInputStream();
+	    b = inputChannel.read();
+	} catch (Exception e) {
+	    System.out.println("Error en envío / recepción de datos");
 	}
-	public void sendByte(int b){
-		try{
-			outputChannel = socketCliente.getOutputStream();
-			outputChannel.write(b);			
-		}catch (Exception e) {
-			System.out.println("Error en envío / recepción de datos");
-		}
-		System.out.println("Envío: " + b); // La información hay que obtenerla fuera del try / catch
+	System.out.println("Recibo: " + b);
+	return b;
+    }
+
+    public void close() {
+	try {
+	    this.socketCliente.close();
+	} catch (Exception e) {
+	    System.out.println("Error de conexión");
 	}
-	public int receiveByte(){
-		int b = 0;
-		try{
-			inputChannel = socketCliente.getInputStream();
-			b = inputChannel.read();			
-		}catch (Exception e) {
-			System.out.println("Error en envío / recepción de datos");
-		}
-		System.out.println("Recibo: " + b);
-		return b;
-	}
-	public void close(){
-		try{
-			this.socketCliente.close();
-		}catch (Exception e) {
-			System.out.println("Error de conexión");
-		}
-	}
+    }
 }
--- Practica1/src/server/ServerTCP.java
+++ Practica1/src/server/ServerTCP.java
@@ -1,4 +1,5 @@
 package server;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
@@ -8,71 +9,79 @@ import utils.ResourceManager;
 
 /**
  * Implementa un servidor de tipo TCP
+ * 
  * @author Sergio Torres
- *
+ * 
  */
-public class ServerTCP implements IServer{
-	private ServerSocket socketServidor;
-	private Socket socketComunicacion;
-	private InputStream inputChannel;
-	private OutputStream outputChannel;
-	private Integer port = 8080;
-	private Integer delay = 0;
-	private ResourceManager resourceManager;
-	
-	public ServerTCP(){
-		try{
-			resourceManager = new ResourceManager();
-			port = new Integer(resourceManager.getResource(Constants.PORT));
-			delay = new Integer(resourceManager.getResource(Constants.DELAY));
-			try{
-				socketServidor = new ServerSocket(this.port);
-			}catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+public class ServerTCP implements IServer {
+    private ServerSocket socketServidor;
+    private Socket socketComunicacion;
+    private InputStream inputChannel;
+    private OutputStream outputChannel;
+    private Integer port = 8080;
+    private Integer delay = 0;
+    private ResourceManager resourceManager;
+
+    public ServerTCP() {
+	try {
+	    resourceManager = new ResourceManager();
+	    port = new Integer(resourceManager.getResource(Constants.PORT));
+	    delay = new Integer(resourceManager.getResource(Constants.DELAY));
+	    try {
+		socketServidor = new ServerSocket(this.port);
+	    } catch (Exception e) {
+		System.out.println(e.getMessage());
+	    }
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
 	}
-	/**
-	 * @param port puerto donde escucha el servidor
-	 * @param delay demora para simular el tratamiento de información por parte del cliente
-	 */
-	public ServerTCP(int port,int delay) {
-		super();
-		this.port = port;
-		this.delay = delay;
-		try{
-			socketServidor = new ServerSocket(this.port);
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+    }
+
+    /**
+     * @param port
+     *            puerto donde escucha el servidor
+     * @param delay
+     *            demora para simular el tratamiento de información por parte
+     *            del cliente
+     */
+    public ServerTCP(int port, int delay) {
+	super();
+	this.port = port;
+	this.delay = delay;
+	try {
+	    socketServidor = new ServerSocket(this.port);
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
 	}
-	public synchronized void handleByte(){
-		try{
-			inputChannel = socketComunicacion.getInputStream();
-			int dato = inputChannel.read();			
-			if (delay>0) {
-				Thread.sleep(delay);// Simula el tratamiento de la información
-			}
-			outputChannel = socketComunicacion.getOutputStream();
-			outputChannel.write(dato);
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+    }
+
+    public synchronized void handleByte() {
+	try {
+	    inputChannel = socketComunicacion.getInputStream();
+	    int dato = inputChannel.read();
+	    if (delay > 0) {
+		Thread.sleep(delay);// Simula el tratamiento de la información
+	    }
+	    outputChannel = socketComunicacion.getOutputStream();
+	    outputChannel.write(dato);
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
 	}
-	public synchronized void accept(){
-		try{
-			socketComunicacion = socketServidor.accept();			
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+    }
+
+    public synchronized void accept() {
+	try {
+	    socketComunicacion = socketServidor.accept();
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
 	}
-	public synchronized void close(){
-		try{
-			socketComunicacion.close();
-		}catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+    }
+
+    public synchronized void close() {
+	try {
+	    socketComunicacion.close();
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
 	}
+    }
 }
