cb63dee1a361559ba24ccd9d3a776897692ab9f6
sergiotorres81@gmail.com <sergiotorres81@gmail.com@e5a0838a-8a8e-4bdd-b3e2-6b6e8bd2478c>
2011-10-12 19:18:59 +0000

    Format code for second practice.

--- Practica2/src/server/SkeletonA.java
+++ Practica2/src/server/SkeletonA.java
@@ -1,34 +1,35 @@
 package server;
 
-import commons.Marshall;
-import commons.UnMarshall;
+import utils.Marshall;
+import utils.UnMarshall;
 
 /**
- * Una posible opción de implementación de Skeleton para tratar las
- * peticiones de objetos ClaseA
+ * Una posible opción de implementación de Skeleton para tratar las peticiones
+ * de objetos ClaseA
+ * 
  * @author Sergio Torres
- *
+ * 
  */
-public class SkeletonA implements Skeleton{
+public class SkeletonA implements Skeleton {
 
-	@Override
-	public void process(UnMarshall unmar, Marshall mar, Object obj) {
-		ClaseA obja = (ClaseA)obj; // Hay que hacer un cast para ajustar el objeto
-		int operationID = unmar.getInt();
-		switch (operationID) {
-		case 1:
-			String key = unmar.getString();
-			int value = unmar.getInt();
-			obja.asociar(key, value);
-			break;
-		case 2:
-			String clave = unmar.getString();
-			int res = obja.obtener(clave);
-			mar.putInt(res);
-			break;
-		default:
-			break;
-		}	
+    public void process(UnMarshall unmar, Marshall mar, Object obj) {
+	ClaseA obja = (ClaseA) obj; // Hay que hacer un cast para ajustar el
+				    // objeto
+	int operationID = unmar.getInt();
+	switch (operationID) {
+	case 1:
+	    String key = unmar.getString();
+	    int value = unmar.getInt();
+	    obja.asociar(key, value);
+	    break;
+	case 2:
+	    String clave = unmar.getString();
+	    int res = obja.obtener(clave);
+	    mar.putInt(res);
+	    break;
+	default:
+	    break;
 	}
+    }
 
 }
--- Practica2/src/server/WorkerMT.java
+++ Practica2/src/server/WorkerMT.java
@@ -2,49 +2,57 @@ package server;
 
 import java.net.Socket;
 
-import commons.Marshall;
-import commons.UnMarshall;
+import utils.Marshall;
+import utils.UnMarshall;
 
 /**
  * Clase encargada de implementar el multithread
+ * 
  * @author Sergio Torres
- *
+ * 
  */
-public  class WorkerMT implements Runnable {
-	private Switcher sw;
-	private Socket so;
-	
-	/**
-	 * El servidor recibe un swicher y un socket
-	 * @param sw
-	 * @param so
-	 */
-	public WorkerMT(Switcher sw, Socket so) {
-		super();
-		this.sw = sw;
-		this.so = so;
+public class WorkerMT implements Runnable {
+    private Switcher sw;
+    private Socket so;
+
+    /**
+     * El servidor recibe un swicher y un socket
+     * 
+     * @param sw
+     * @param so
+     */
+    public WorkerMT(Switcher sw, Socket so) {
+	super();
+	this.sw = sw;
+	this.so = so;
+    }
+
+    @Override
+    public void run() {
+	try {
+	    sw.procesar(new UnMarshall(so.getInputStream()),
+		    new Marshall(so.getOutputStream()));
+	    so.close();
+	} catch (Exception e) {
+	    System.out.println("Algo va mal");
 	}
-	@Override
-	public void run() {
-		try{
-			sw.procesar(new UnMarshall(so.getInputStream()), new Marshall(so.getOutputStream()));
-			so.close();
-		}catch (Exception e) {
-			System.out.println("Algo va mal");
-		}
-		
-	}
-	public void setSw(Switcher sw) {
-		this.sw = sw;
-	}
-	public Switcher getSw() {
-		return sw;
-	}
-	public void setSo(Socket so) {
-		this.so = so;
-	}
-	public Socket getSo() {
-		return so;
-	}
+
+    }
+
+    public void setSw(Switcher sw) {
+	this.sw = sw;
+    }
+
+    public Switcher getSw() {
+	return sw;
+    }
+
+    public void setSo(Socket so) {
+	this.so = so;
+    }
+
+    public Socket getSo() {
+	return so;
+    }
 
 }
