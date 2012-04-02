ff47fa4e9960a2658316e4932c7349a8e8d506b5
sergiotorres81@gmail.com <sergiotorres81@gmail.com@e5a0838a-8a8e-4bdd-b3e2-6b6e8bd2478c>
2012-04-02 06:31:44 +0000

    New inheritance between stubs

--- trunk/Practica2/src/client/StubB.java
+++ trunk/Practica2/src/client/StubB.java
@@ -10,35 +10,35 @@ import utils.IOperations;
  * 
  */
 public class StubB extends StubA implements IOperations {
-    public StubB() {
-	super();
-    }
+	public StubB() {
+		super();
+	}
 
-    public StubB(String host, int port, int idObjeto, int idSkeleton) {
-	super(host, port, idObjeto, idSkeleton);
-    }
+	public StubB(String host, int port, int idObjeto, int idSkeleton) {
+		super(host, port, idObjeto, idSkeleton);
+	}
 
-    public int subtract(String op1, String op2) {
-	connect();
-	mar.putInt(idObjeto);
-	mar.putInt(idSkeleton);
-	mar.putInt(4); // Resta tiene asignado el id de operación 4
-	mar.putString(op1);
-	mar.putString(op2);
-	int res = unmar.getInt();
-	disconnect();
-	return res;
-    }
+	public int subtract(String op1, String op2) {
+		connect();
+		mar.putInt(objectId);
+		mar.putInt(skeletonId);
+		mar.putInt(4); // Resta tiene asignado el id de operación 4
+		mar.putString(op1);
+		mar.putString(op2);
+		int res = unmar.getInt();
+		disconnect();
+		return res;
+	}
 
-    public int add(String op1, String op2) {
-	connect();
-	mar.putInt(idObjeto);
-	mar.putInt(idSkeleton);
-	mar.putInt(3); // Suma tiene asignado el id de operación 3
-	mar.putString(op1);
-	mar.putString(op2);
-	int res = unmar.getInt();
-	disconnect();
-	return res;
-    }
+	public int add(String op1, String op2) {
+		connect();
+		mar.putInt(objectId);
+		mar.putInt(skeletonId);
+		mar.putInt(3); // Suma tiene asignado el id de operación 3
+		mar.putString(op1);
+		mar.putString(op2);
+		int res = unmar.getInt();
+		disconnect();
+		return res;
+	}
 }
